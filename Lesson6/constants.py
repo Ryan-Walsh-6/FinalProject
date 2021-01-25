91fcac3c41a46eccd0db1d19ff039639167dadc9
Ryan-Walsh-6 <74618988+Ryan-Walsh-6@users.noreply.github.com>
2021-01-24 21:59:00 -0500

    Create constants.py

---
+++ Lesson6/constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Ryan Walsh
+# Created on: December 2020
+# This program is the constants file for The Snake Game game
+
+# PyBadge screen size is 160x128 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 128
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SNAKE_SIZE = 16
+TOTAL_NUMBER_OF_APPLES = 5
+FPS = 60
+SNAKE_MOVEMENT_SPEED = 1
+
+# using for button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
