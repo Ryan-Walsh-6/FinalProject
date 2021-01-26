d78e07031eb015b954cd46eb3f1a13348421ce6c
Ryan-Walsh-6 <74618988+Ryan-Walsh-6@users.noreply.github.com>
2021-01-26 16:07:54 -0500

    Update code.py

--- Works/code.py
+++ Works/code.py
@@ -6,10 +6,122 @@
 
 import ugame
 import stage
-import random
 import time
+import random
+import board
+import supervisor
 
 import constants
+
+def splash_scene():
+    # this function is the menu scene
+
+    # get sound ready
+    coin_sound = open("coin.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+    sound.play(coin_sound)
+
+    # image banks for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    # sets the background to image 0 in the image bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
+
+    # used this program to split the image into tile:
+    #    https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+
+
+    # create a stage for the background to show up on
+    #    and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers, items show up in order
+    game.layers = [background]
+    # render the background and initial location of sprite list
+    # most likely you will only render background once per scene
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # Wait for 2 seconds
+        time.sleep(2.0)
+        menu_scene()
+
+def menu_scene():
+    # this function is the menu scene
+
+    # image banks for CircuitPython
+    image_bank_mt_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # add text objects
+    text = []
+    text1 = stage.Text(width=29, height=12, font=None, 
+    palette=constants.RED_PALETTE, buffer=None)
+    text1.move(20,10)
+    text1.text("The Snake Game")
+    text.append(text1)
+    
+    text2 = stage.Text(width=29, height=12, font=None, 
+    palette=constants.RED_PALETTE, buffer=None)
+    text2.move(40,110)
+    text2.text("PRESS START")
+    text.append(text2)
+
+    # sets the background to image 0 in the image bank
+    background = stage.Grid(image_bank_mt_background, constants.SCREEN_X,
+                            constants.SCREEN_Y)
+
+    # create a stage for the background to show up on
+    #    and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+    # set the layers, items show up in order
+    game.layers = text + [background]
+    # render the background and initial location of sprite list
+    # most likely you will only render background once per scene
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # get user input
+        keys = ugame.buttons.get_pressed()
+
+        # Start button selected
+        if keys & ugame.K_START != 0:
+            game_scene()
+
+        # update game logic
+        game.tick() # wait until refresh rate finishes
 
 def game_scene():
    def show_apple():
@@ -27,6 +139,14 @@ def game_scene():
    sound = ugame.audio
    sound.stop()
    sound.mute(False)
+   
+   score = 0
+
+   score_text = stage.Text(width=29, height=14)
+   score_text.clear()
+   score_text.cursor(0,0)
+   score_text.move(1,1)
+   score_text.text("Score: {0}".format(score))
     # this function is the main game game_scene
    snakes = []
    def show_snake(x , y):
@@ -78,7 +198,6 @@ def game_scene():
    wormCoords = [{'x': snakes[0].x , 'y': snakes[0].y}]
    newHead = {'x':snakes[0].x, 'y' :snakes[0].y}
    HEAD = 0       
-   score = 0 
 # repeat forever, game loop
    while True:
        # get user input
@@ -104,11 +223,7 @@ def game_scene():
            left_button = "button_up"
            up_button = "button_up"
            down_button = "button_pressed"
-  
-       
-  
-  
-  
+
        if keys & ugame.K_X:
            pass
        if keys & ugame.K_O:
@@ -134,6 +249,11 @@ def game_scene():
                            sound.play(apple_crunch)
                         # you hit an alien
                            score = score + 1
+                           score_text.clear()
+                           score_text.cursor(0,0)
+                           score_text.move(1,1)
+                           score_text.text("Score: {0}".format(score))
+
                            apples[apple_number].move(constants.OFF_SCREEN_X,
                                                   constants.OFF_SCREEN_Y)
                            show_apple()
@@ -192,14 +312,14 @@ def game_scene():
        game.tick() # wait until refresh rate finishes
 
 def game_over_scene(final_score):
-    # this function is the game over scene
+     # this function is the game over scene
            
    # turn off sound from last scene
    sound = ugame.audio
    sound.stop()
            
    # image banks for Circuitpython
-   image_bank_2 = stage.Bank.from_bmp16("PyBadge_bank_color_template.bmp")
+   image_bank_2 = stage.Bank.from_bmp16("mt_game_studio.bmp")
            
    # sets the background to image 0 in the image bank
    background = stage.Grid(image_bank_2, constants.SCREEN_GRID_X,
@@ -228,7 +348,7 @@ def game_over_scene(final_score):
    #     ad set the frame rate to 60fps
    game = stage.Stage(ugame.display, constants.FPS)
    # set layers of all sprites, items show up in order
-   game.layers = [background]
+   game.layers = text + [background]
    # render all sprites
    #   most likely you will only render the background once per game scene
    game.render_block()
@@ -245,6 +365,5 @@ def game_over_scene(final_score):
            # update game logic
        game.tick() #  wait until refresh rate finishes
 
-
 if __name__ == "__main__":
-    game_scene()
+    splash_scene()
